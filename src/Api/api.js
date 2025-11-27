f5ed0deed1f53811f8e21440ed94d1def702d097
McOriginal <originalmc223@gmail.com>
2025-11-27 07:57:51 +0000

    correction de URL API

--- src/Api/api.js
+++ src/Api/api.js
@@ -5,7 +5,7 @@ const api = axios.create({
   baseURL:
     process.env.REACT_APP_API_URL ||
     // 'http://localhost:5000/api',
-    'https://bawckend-cabinet-medical-le-guide.onrender.com/api',
+    'https://backend-cabinet-medical-le-guide.onrender.com/api',
 
   headers: {
     'Content-Type': 'application/json',
